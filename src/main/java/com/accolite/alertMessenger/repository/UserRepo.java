0ec01846fae2331596dd37be331debe71688e4a1
yashAccolite <126050815+yashAccolite@users.noreply.github.com>
2023-04-10 16:04:25 +0530

    Update UserRepo.java
    User Repo Changes

--- src/main/java/com/accolite/alertMessenger/repository/UserRepo.java
+++ src/main/java/com/accolite/alertMessenger/repository/UserRepo.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface UserRepo extends JpaRepository<User, UUID> {
+public interface UserRepo extends JpaRepository<User, Long> {
     User findByUserName(String userName);
 }
