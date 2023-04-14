0da2e97544b0df357950f912b7d98556649ef369
mridulsemwal <mridul.semwal@accolitedigital.com>
2023-04-14 20:51:17 +0530

    Refactored my code

---
+++ src/main/java/com/accolite/alertMessenger/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.accolite.alertMessenger.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConfigController {
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    ConfigController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void sendMessage(String message){
+        System.out.println(message);
+        this.template.convertAndSend("/message",  message);
+    }
+}
