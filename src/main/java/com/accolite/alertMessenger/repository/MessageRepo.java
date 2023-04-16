f028f308ad4e326f4850dc90c6068026e77dda6c
mridulsemwal <mridul.semwal@accolitedigital.com>
2023-04-16 15:11:33 +0530

    Added New APIs for read and unread messages

--- src/main/java/com/accolite/alertMessenger/repository/MessageRepo.java
+++ src/main/java/com/accolite/alertMessenger/repository/MessageRepo.java
@@ -10,7 +10,10 @@ import java.util.List;
 @Repository
 public interface MessageRepo extends JpaRepository<Message, Integer> {
 
-    @Query(value = "select * from message where is_published = 1", nativeQuery = true)
-    public List<Message> getMessagesForUser();
+    @Query(value = "select * from message where is_published = 1 and acknowledge = 'NO'", nativeQuery = true)
+    public List<Message> getUnreadMessagesForUser();
+
+    @Query(value = "select * from message where is_published = 1 and acknowledge = 'YES'", nativeQuery = true)
+    public List<Message> getReadMessagesForUser();
 
 }
